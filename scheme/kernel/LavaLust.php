7c9c887d0dc0c8fec9f2f6dca859f736805d1ff3
preelleei <aprellejoyramos@gmail.com>
2026-09-03 14:05:39 +0800

    Enable production error reporting for deployment debugging

--- scheme/kernel/LavaLust.php
+++ scheme/kernel/LavaLust.php
@@ -119,8 +119,8 @@ switch (strtolower(config_item('environment')))
 
 	case 'testing':
 	case 'production':		
-		ini_set('display_errors', 0);
-		error_reporting(0);
+		ini_set('display_errors', 1);
+		error_reporting(-1);
 		_handlers();
 	break;
 
